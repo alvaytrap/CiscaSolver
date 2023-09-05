35f451572f5acd507fc3070959960c41274bb02c
alvaytrap <alvay.trap@gmail.com>
2023-09-05 23:44:23 +0200

    Improved github pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/ciscacolver",
+  basePath: "/ciscacsolver",
   reactStrictMode: true,
   swcMinify: true,
   modularizeImports: {
